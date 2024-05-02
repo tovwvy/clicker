0f5227fb53f5c50b6c8de593830ed0e89ad0eca4
tovwvy <104623411+tovwvy@users.noreply.github.com>
2024-05-02 13:54:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.image import Image
 from kivy.clock import Clock
 from kivy.uix.progressbar import ProgressBar
-from kivy.animation import Animation
-
 
 kivy.require('1.11.1')
 
@@ -14,15 +12,12 @@ class CookieClicker(App):
     def __init__(self, **kwargs):
         super(CookieClicker, self).__init__(**kwargs)
         self.cookie_count = 0
-        self.image_scale = 1.0  # Додавання атрибуту image_scale тут
         self.hand_count = 0
         self.granny_count = 0
-        self.factory_count = 0
         self.click_count = 0  
         self.baked_cookie_count = 0  
         self.level = 1  # Ініціалізуємо атрибут рівня
 
-     
     def build(self):
         self.layout = RelativeLayout()
 
@@ -32,7 +27,7 @@ class CookieClicker(App):
         # Кнопка печива
         self.cookie_button = Image(source='png-clipart-cookie-clicker-clicker-heroes-incremental-game-cookie-game-baked-goods-thumbnail.png', 
                                    size_hint=(None, None), size=(300, 300), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        self.cookie_button.bind(on_touch_down=self.scale_image)
+        self.cookie_button.bind(on_touch_down=self.on_click)
         self.layout.add_widget(self.cookie_button)
        
         # Лейбли та іконки
@@ -50,9 +45,6 @@ class CookieClicker(App):
 
         self.granny_label = Label(text="Grannies: 0", font_size=20, size_hint=(None, None), size=(150, 50), pos_hint={'right': 0.95, 'top': 0.7})
         self.layout.add_widget(self.granny_label)
-
-        self.factory_label = Label(text="Factories: 0", font_size=20, size_hint=(None, None), size=(150, 50), pos_hint={'right': 0.95, 'top': 0.65})
-        self.layout.add_widget(self.factory_label)
 
         self.level_label = Label(text="Level: 1", font_size=20, size_hint=(None, None), size=(150, 50), pos_hint={'right': 0.95, 'top': 0.6})
         self.layout.add_widget(self.level_label)
@@ -72,10 +64,6 @@ class CookieClicker(App):
         self.buy_granny_icon.bind(on_touch_down=self.buy_granny)  
         self.layout.add_widget(self.buy_granny_icon)
 
-        self.buy_factory_icon = Image(source='5095911.png', size_hint=(None, None), size=(50, 50), pos_hint={'right': 0.95, 'top': 0.25})
-        self.buy_factory_icon.bind(on_touch_down=self.buy_factory)  
-        self.layout.add_widget(self.buy_factory_icon)
-
         # Лейбли для відображення кількості
         self.hand_count_label = Label(text="0", size_hint=(None, None), size=(50, 50), pos_hint={'right': 0.9, 'top': 0.45})
         self.layout.add_widget(self.hand_count_label)
@@ -83,15 +71,42 @@ class CookieClicker(App):
         self.granny_count_label = Label(text="0", size_hint=(None, None), size=(50, 50), pos_hint={'right': 0.9, 'top': 0.35})
         self.layout.add_widget(self.granny_count_label)
 
-        self.factory_count_label = Label(text="0", size_hint=(None, None), size=(50, 50), pos_hint={'right': 0.9, 'top': 0.25})
-        self.layout.add_widget(self.factory_count_label)
-
         Clock.schedule_interval(self.automatic_click, 1)
         Clock.schedule_interval(self.level_up, 1)
 
         return self.layout
 
+    def on_click(self, instance, touch):
+        if instance.collide_point(*touch.pos):
+            # Increment click count and update label
+            self.click_count += 1
+            self.counter_label.text = "Clicks: {}".format(self.click_count)
 
+            # Increment cookie count and update label every 10 clicks
+            if self.click_count % 10 == 0:
+                self.cookie_count += 2
+                self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
+
+                self.baked_cookie_count += 2
+                self.baked_cookie_label.text = "Baked Cookies: {}".format(self.baked_cookie_count)
+
+    def buy_hand(self, instance, touch):
+        if self.cookie_count >= 10:
+            self.cookie_count -= 10
+            self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
+            self.hand_count += 1
+            self.hand_label.text = "Hands: {}".format(self.hand_count)
+            self.hand_count_label.text = str(self.hand_count)  
+
+    def buy_granny(self, instance, touch):
+        if self.cookie_count >= 30:
+            self.cookie_count -= 30
+            self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
+            self.granny_count += 1
+            self.granny_label.text = "Grannies: {}".format(self.granny_count)
+            self.granny_count_label.text = str(self.granny_count)
+
+ 
 
     def level_up(self, dt):
         if self.cookie_count >= self.level * 100:
@@ -101,57 +116,6 @@ class CookieClicker(App):
             self.level_label.text = "Level: {}".format(self.level)
             self.level_progress.max = self.level * 100
             self.level_progress.disabled = True
-
-    def on_click(self, instance, touch):
-        if touch is not None:
-            if self.cookie_button.collide_point(*touch.pos):
-                self.click_count += 1
-                self.counter_label.text = "Clicks: {}".format(self.click_count)
-
-            if self.click_count % 10 == 0:
-                self.cookie_count += 2
-                self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
-
-                self.baked_cookie_count += 2
-                self.baked_cookie_label.text = "Baked Cookies: {}".format(self.baked_cookie_count)
-
-
-    def scale_image(self, instance, touch):
-        if instance.collide_point(*touch.pos):
-            # Створення анімації для збільшення зображення
-            animation = Animation(scale=self.image_scale * 1.2, duration=0.2) + Animation(scale=self.image_scale, duration=0.2)
-            animation.start(instance)
-
-
-    def buy_hand(self, instance, touch):
-        if touch:
-            if self.buy_hand_icon.collide_point(*touch.pos):
-                if self.cookie_count >= 10:
-                    self.cookie_count -= 10
-                    self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
-                    self.hand_count += 1
-                    self.hand_label.text = "Hands: {}".format(self.hand_count)
-                    self.hand_count_label.text = str(self.hand_count)  
-
-    def buy_granny(self, instance, touch):
-        if touch:
-            if self.buy_granny_icon.collide_point(*touch.pos):
-                if self.cookie_count >= 30:
-                    self.cookie_count -= 30
-                    self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
-                    self.granny_count += 1
-                    self.granny_label.text = "Grannies: {}".format(self.granny_count)
-                    self.granny_count_label.text = str(self.granny_count)  
-
-    def buy_factory(self, instance, touch):
-        if touch:
-            if self.buy_factory_icon.collide_point(*touch.pos):
-                if self.cookie_count >= 50:
-                    self.cookie_count -= 50
-                    self.cookie_label.text = "Cookies: {}".format(self.cookie_count)
-                    self.factory_count += 1
-                    self.factory_label.text = "Factories: {}".format(self.factory_count)
-                    self.factory_count_label.text = str(self.factory_count)
 
     def automatic_click(self, dt):
         hand_cookies_produced = 0
